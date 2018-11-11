1c46f184ce8ea990d53150c31f36db2e8ef4e29b
Tony Han <itony9401@live.com>
2018-11-11 03:16:21 -0800

    consolidated into one file

--- scripts/snappy.py
+++ scripts/snappy.py
@@ -1,10 +1,31 @@
+import boto3
 import click
-from utils import filter_instances
-from setup import (
-    session,
-    ec2,
-    set_defaults
-)
+
+session = boto3.Session()
+ec2 = session.resource('ec2')
+
+def set_defaults(region=None, profile=None, **kwargs):
+    if profile:
+        global session
+        session = boto3.Session(profile_name=profile)
+
+    if region:
+        global ec2
+        ec2 = session.resource('ec2', region_name=region)
+
+    return
+
+
+def filter_instances(project):
+    instances = []
+
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
 
 
 @click.group()
