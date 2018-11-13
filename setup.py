1cb01f6839bf6a1c802441df497db15f17e07632
Tony Han <itony9401@live.com>
2018-11-12 19:17:45 -0800

    added setup script

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='snappy',
+    version='0.1',
+    author='Tony Han',
+    author_email='itony9401@live.com',
+    summary='Snappy is a CLI that uses `boto3` to manage AWS EC2 instance snapshots.',
+    license='GPLv3+',
+    packages=['snappy'],
+    url='https://github.com/tyh835/snappy',
+    install_requires=[
+        'click',
+        'boto3'
+    ],
+    entry_points='''
+        [console_scripts]
+        snappy=snappy:cli
+    '''
+)
